6ea96c317bc9a23a0a5f7fdf85e565f1d3da83f0
Maaxboon <maaxwelokoth@gmail.com>
2024-10-24 07:19:40 +0300

    Adds dogyears.js

---
+++ bloomtech/dogYears.js
@@ -0,0 +1,35 @@
+//  This is my current age
+const myAge = 24;
+
+// First two years of a dog
+let earlyYears = 2;
+
+// Calculates the age of the dog in it's first two years
+earlyYears *= 10.5;
+
+// Later years of a dog, remove two from human years
+let laterYears = myAge - 2;
+
+// Later years of a dog are four each
+laterYears *= 4;
+
+// Prints early years
+console.log(earlyYears);
+
+// Prints later years
+console.log(laterYears);
+
+// Calculates my age in dog years
+const myAgeInDogYears = earlyYears + laterYears;
+
+// Creates a variable with my name
+const myName = "Maaxboon".toLowerCase();
+
+// prints my age in dog years
+console.log(
+  `My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`
+);
+
+console.log("hi!".length);
+
+console.log("Hello world");
